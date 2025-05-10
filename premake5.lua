435b552314d00cc1f0b8f4c762b9745450778042
NishiOwO <nishi@nishi.boats>
2025-05-10 13:57:24 +0900

    init

---
+++ premake5.lua
@@ -0,0 +1,61 @@
+workspace("ircd2-mp")
+        configurations({
+                "Debug",
+                "Release"
+        })
+        platforms({
+                "Native",
+                "Win32",
+                "Win64"
+        })
+        defaultplatform("Native")
+
+filter("platforms:Win32")
+        system("windows")
+        architecture("x86")
+        gccprefix("i686-w64-mingw32-")
+
+filter("platforms:Win64")
+        system("windows")
+        architecture("x86_64")
+        gccprefix("x86_64-w64-mingw32-")
+
+filter({})
+
+project("Common")
+	kind("StaticLib")
+	files("src/common/*.c")
+	includedirs({
+		"include/common",
+		"include/server",
+		"include"
+	})
+
+project("Server")
+	kind("ConsoleApp")
+	targetname("ircd")
+	files("src/server/*.c")
+	removefiles({
+		"src/server/config_read.c",
+		"src/server/fileio.c",
+	})
+	includedirs({
+		"include/common",
+		"include/server",
+		"include"
+	})
+	links("Common")
+	filter("system:windows")
+	links("wsock32")
+	filter({})
+	filter("configurations:Debug")
+		defines({
+			"DEBUG"
+		})
+		symbols("On")
+	filter("configurations:Release")
+		defines({
+			"NDEBUG"
+		})
+		optimize("On")
+	filter({})
